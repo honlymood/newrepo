b256da99b29a1d32af6d01147998a5d429285bcd
Fredric Nelson <162432886+honlymood@users.noreply.github.com>
2024-03-06 11:32:11 +0200

    Create maincod.js

---
+++ maincod.js
@@ -0,0 +1,23 @@
+if(this._handler){
+			if(index > -1){
+				return this.table.rowManager.getDisplayRows(index);
+			}else{
+				return this.activeRows();
+			}
+
+var el = document.createElement("div");
+		
+		el.classList.add("tabulator-table");
+		el.setAttribute("role", "rowgroup");
+		
+		return el;
+	}
+if(placeholder){	
+			let el = document.createElement("div");
+			el.classList.add("tabulator-placeholder");
+			
+			if(typeof placeholder == "string"){
+				let contents = document.createElement("div");
+				contents.classList.add("tabulator-placeholder-contents");
+				contents.innerHTML = placeholder;
+				
